ceedda1629b4ed85512cb7cb555842e9e83ab19b
jin123457 <jinsol9909@gmail.com>
2025-09-03 15:11:38 +0900

    feat: lazy loading을 사용하여 Home 및 Search 컴포넌트 로딩 최적화 및 webpack 설정에서 splitChunks 설정

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -56,6 +56,9 @@ module.exports = {
     ]
   },
   optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
     minimize: true
   }
 };
