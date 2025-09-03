21dd431dd4e02cf2a943a87d1b71e269f20e1952
jin123457 <jinsol9909@gmail.com>
2025-09-03 13:38:05 +0900

    refactor: 제목 요소를 h4에서 h3으로 변경하여 접근성 증진

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -10,7 +10,7 @@ const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
     <div className={styles.featureItem}>
       <video src={imageSrc} className={styles.featureImage} loop muted autoPlay playsInline></video>
       <div className={styles.featureTitleBg}></div>
-      <h4 className={styles.featureTitle}>{title}</h4>
+      <h3 className={styles.featureTitle}>{title}</h3>
     </div>
   );
 };
