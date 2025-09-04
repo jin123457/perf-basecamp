0a2f80fa8c1510973dd0d096bf53f19ea65601e6
jin123457 <jinsol9909@gmail.com>
2025-09-04 15:34:52 +0900

    refactor: 다양한 해상도에 맞춘 hero 이미지 추가 및 웹 최적화

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,12 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import heroImageWebp from '../../assets/images/hero.webp';
+import heroImage from '../../assets/images/hero.jpg';
+import heroImageXL from '../../assets/images/hero-xl.webp';
+import heroImageL from '../../assets/images/hero-l.webp';
+import heroImageM from '../../assets/images/hero-m.webp';
+import heroImageS from '../../assets/images/hero-s.webp';
+import heroImageXS from '../../assets/images/hero-xs.webp';
 import trendingMp4 from '../../assets/images/trending.mp4';
 import findMp4 from '../../assets/images/find.mp4';
 import freeMp4 from '../../assets/images/free.mp4';
@@ -23,7 +27,11 @@ const Home = () => {
     <>
       <section className={styles.heroSection}>
         <picture>
-          <source type="image/webp" srcSet={heroImageWebp} />
+          <source type="image/webp" srcSet={heroImageXS} media="(max-width:480px)" />
+          <source type="image/webp" srcSet={heroImageS} media="(max-width:768px)" />
+          <source type="image/webp" srcSet={heroImageM} media="(max-width:1024px)" />
+          <source type="image/webp" srcSet={heroImageL} media="(max-width:1280px)" />
+          <source type="image/webp" srcSet={heroImageXL} media="(max-width:1920px)" />
           <img
             className={styles.heroImage}
             src={heroImage}
