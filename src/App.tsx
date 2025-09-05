145f1b55f5e0a22dce4a6384996641bfec1060cb
jin123457 <jinsol9909@gmail.com>
2025-09-05 09:33:21 +0900

    refactor: Home 컴포넌트를 lazy 로딩에서 일반 import로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-const Home = lazy(() => import('./pages/Home/Home'));
+import Home from './pages/Home/Home';
 const Search = lazy(() => import('./pages/Search/Search'));
 
 import NavBar from './components/NavBar/NavBar';
